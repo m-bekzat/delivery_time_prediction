4567fd58af29c2f64890d5e3754075595bdc15e0
Bekzat Minazhatov <mn.bekzat@gmail.com>
2026-05-24 00:40:39 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -10,26 +10,9 @@ st.title("Delivery Time Prediction")
 st.write("This app predicts delivery time in minutes.")
 
 distance_km = st.number_input("Distance in km", min_value=1.0, max_value=50.0, value=5.0)
-courier_experience_years = st.number_input(
-    "Courier experience years",
-    min_value=0.0,
-    max_value=20.0,
-    value=2.0,
-    step=0.5
-)
-hour = st.slider(
-    "Order Time (Hour of Day)",
-    0,
-    23,
-    12
-)
-
-weekday = st.slider(
-    "Day of Week (1 = Monday, 7 = Sunday)",
-    1,
-    7,
-    1
-)
+courier_experience_years = st.number_input("Courier experience years", min_value=0.0, max_value=20.0, value=2.0)
+hour = st.slider("Hour", 0, 23, 12)
+weekday = st.slider("Weekday", 0, 6, 1)
 
 weather = st.selectbox("Weather", ["sunny", "rain", "snow"])
 traffic_level = st.selectbox("Traffic level", ["low", "medium", "high"])
@@ -41,12 +24,19 @@ input_data = pd.DataFrame({
     "courier_experience_years": [courier_experience_years],
     "hour": [hour],
     "weekday": [weekday],
-    "weather": [weather],
-    "traffic_level": [traffic_level],
-    "restaurant_load": [restaurant_load],
-})
 
-input_data = pd.get_dummies(input_data, drop_first=True)
+    "weather_rain": [1 if weather == "rain" else 0],
+    "weather_snow": [1 if weather == "snow" else 0],
+    "weather_sunny": [1 if weather == "sunny" else 0],
+
+    "traffic_level_low": [1 if traffic_level == "low" else 0],
+    "traffic_level_medium": [1 if traffic_level == "medium" else 0],
+    "traffic_level_high": [1 if traffic_level == "high" else 0],
+
+    "restaurant_load_low": [1 if restaurant_load == "low" else 0],
+    "restaurant_load_medium": [1 if restaurant_load == "medium" else 0],
+    "restaurant_load_high": [1 if restaurant_load == "high" else 0],
+})
 
 input_data = input_data.reindex(
     columns=model.feature_names_in_,
